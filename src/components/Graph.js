6804c7acda3bf0cf6374b8b0b9d5e351ebcd8fd6
noa <aggarwal.neeraj141@gmail.com>
2020-11-20 02:33:56 -0800

    Always show username

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ForceGraph3D } from "react-force-graph";
+import SpriteText from "three-spritetext";
 
 function Graph({ graph }) {
   return (
@@ -15,6 +16,16 @@ function Graph({ graph }) {
       linkWidth={1.5}
       linkDirectionalParticles={4}
       linkDirectionalParticleWidth={1}
+      nodeThreeObject={(node) => {
+        const sprite = new SpriteText(node.name);
+        sprite.color = node.color;
+        sprite.textHeight = 4;
+        sprite.position.y = -8;
+        return sprite;
+      }}
+      nodeThreeObjectExtend={true}
+      nodeRelSize={3}
+      nodeLabel={""}
     />
   );
 }
