70faf577e38cef45c2f3e6323db7ba5da59d9a54
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-11-21 05:44:55 +0000

    🖼️ Add landing page (#4)

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../css/Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img src="./images/navImg.png" alt="pineapplelol logo" />
+    </div>
+  );
+}
+
+export default Nav;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Input } from "antd";
+import { useHistory } from "react-router-dom";
+
+import Nav from "../components/Nav";
+import "../css/Home.css";
+
+const { Search } = Input;
 
 function Home() {
-  return <>venmo social network visualizer</>;
+  const history = useHistory();
+
+  return (
+    <div className="landing">
+      <Nav />
+      <div className="landing-content">
+        <div className="landing-text">
+          <h1>Explore</h1>
+          <h2>your Venmo network.</h2>
+          <p>Enter a username to explore their payment network.</p>
+          <Search
+            placeholder="Venmo username"
+            onSearch={(value) => {
+              history.push(`/${value}`);
+            }}
+            size="large"
+            enterButton
+          />
+        </div>
+        <div className="landing-img">
+          <img src="./images/space.svg" alt="space" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
