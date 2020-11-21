5439d794b0f4699a364854ff2516a8aaef948b3b
noa <aggarwal.neeraj141@gmail.com>
2020-11-20 21:42:14 -0800

    Add redirection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Input } from "antd";
+import { useHistory } from "react-router-dom";
 
 import Nav from "../components/Nav";
 import "../css/Home.css";
@@ -7,18 +8,20 @@ import "../css/Home.css";
 const { Search } = Input;
 
 function Home() {
+  const history = useHistory();
+
   return (
     <div className="landing">
       <Nav />
       <div className="landing-content">
         <div className="landing-text">
-          <h1>Discover</h1>
+          <h1>Explore</h1>
           <h2>your Venmo network.</h2>
-          <p>Enter a username to learn about their payment network.</p>
+          <p>Enter a username to explore their payment network.</p>
           <Search
             placeholder="Venmo username"
             onSearch={(value) => {
-              console.log(value);
+              history.push(`/${value}`);
             }}
             size="large"
             enterButton
