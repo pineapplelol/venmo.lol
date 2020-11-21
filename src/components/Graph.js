208c8819c16acd29e60dc57a4e581d28bfd7dc15
noa <aggarwal.neeraj141@gmail.com>
2020-11-21 15:29:28 -0800

    Add cusor update on node hover

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,38 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
 import SpriteText from "three-spritetext";
 import { useHistory } from "react-router-dom";
 
 function Graph({ graph }) {
   const history = useHistory();
+  const [cursor, setCursor] = useState("default");
 
   return (
-    <ForceGraph3D
-      graphData={graph}
-      nodeId="name"
-      linkSource="from"
-      linkTarget="to"
-      backgroundColor="#000000"
-      enableNodeDrag={false}
-      nodeAutoColorBy="name"
-      linkAutoColorBy="from"
-      linkWidth={1.5}
-      linkDirectionalParticles={4}
-      linkDirectionalParticleWidth={1}
-      nodeThreeObject={(node) => {
-        const sprite = new SpriteText(node.name);
-        sprite.color = node.color;
-        sprite.textHeight = 4;
-        sprite.position.y = -8;
-        return sprite;
-      }}
-      nodeThreeObjectExtend={true}
-      nodeRelSize={3}
-      onNodeClick={(value) => {
-        history.push(`/${value.name}`);
-      }}
-      nodeLabel={""}
-    />
+    <div style={{ cursor: cursor }}>
+      <ForceGraph3D
+        graphData={graph}
+        nodeId="name"
+        linkSource="from"
+        linkTarget="to"
+        backgroundColor="#000000"
+        enableNodeDrag={false}
+        nodeAutoColorBy="name"
+        linkAutoColorBy="from"
+        linkWidth={1.5}
+        linkDirectionalParticles={4}
+        linkDirectionalParticleWidth={1}
+        nodeThreeObject={(node) => {
+          const sprite = new SpriteText(node.name);
+          sprite.color = node.color;
+          sprite.textHeight = 4;
+          sprite.position.y = -8;
+          return sprite;
+        }}
+        nodeThreeObjectExtend={true}
+        nodeRelSize={3}
+        onNodeClick={(value) => {
+          history.push(`/${value.name}`);
+        }}
+        nodeLabel={""}
+        onNodeHover={(node) =>
+          node ? setCursor("pointer") : setCursor("default")
+        }
+      />
+    </div>
   );
 }
 
