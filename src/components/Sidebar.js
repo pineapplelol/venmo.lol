0bc3001e9f9c6426d1b8c814f7586d97d2527841
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-11-22 01:10:44 +0000

    🧨 Add sidebar information, improve interaction (#6)

---
+++ src/components/Sidebar.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Button, Collapse, Layout, Input, List } from "antd";
+import { useHistory } from "react-router-dom";
+
+import "../css/Sidebar.css";
+
+const { Panel } = Collapse;
+const { Search } = Input;
+const { Content, Sider } = Layout;
+
+const ListItem = List.Item;
+const ListItemMeta = List.Item.Meta;
+
+function Sidebar({ username, userDegrees, transactions }) {
+  const history = useHistory();
+  const users = Object.keys(userDegrees);
+  const [searchUser, setSearchUser] = useState(username);
+
+  const directToUser = (value) => history.push(`/${value}`);
+
+  return (
+    <Sider width={"30%"} theme="light">
+      <div className="sidebar-content">
+        <Content>
+          <h1>{username}</h1>
+          <Search
+            value={searchUser}
+            onSearch={directToUser}
+            onChange={(e) => setSearchUser(e.target.value)}
+            enterButton
+            width={"80%"}
+          />
+          <div className="sidebar-information">
+            <Collapse accordion>
+              <Panel header="Users in Graph" key="users" extra={users.length}>
+                <List
+                  dataSource={users}
+                  renderItem={(user) => (
+                    <Button
+                      className="user-list-card"
+                      onClick={() => {
+                        directToUser(user);
+                      }}
+                      block
+                    >
+                      <ListItem>
+                        <ListItemMeta
+                          title={
+                            <div className="user-list-row">
+                              {user}
+                              <div>Depth: {userDegrees[user]}</div>
+                            </div>
+                          }
+                        />
+                      </ListItem>
+                    </Button>
+                  )}
+                />
+              </Panel>
+              <Panel
+                header="Transactions"
+                key="transactions"
+                extra={transactions.length}
+              >
+                <List
+                  dataSource={transactions}
+                  renderItem={(t) => (
+                    <ListItem>
+                      <ListItemMeta
+                        title={
+                          <div className="user-list-row">
+                            {t.transactionType === "charged"
+                              ? `${t.recipient} charged ${t.sender}`
+                              : `${t.sender} paid ${t.recipient}`}
+                            <div className="user-list-row-description">
+                              {t.date}
+                            </div>
+                          </div>
+                        }
+                        description={t.message}
+                      />
+                    </ListItem>
+                  )}
+                />
+              </Panel>
+            </Collapse>
+          </div>
+        </Content>
+      </div>
+    </Sider>
+  );
+}
+
+export default Sidebar;
--- src/pages/UserGraph.js
+++ src/pages/UserGraph.js
@@ -1,35 +1,71 @@
 import React, { useState, useEffect } from "react";
+import { Layout } from "antd";
+
 import Graph from "../components/Graph";
+import Sidebar from "../components/Sidebar";
 
 import { getUserTransactions } from "../util/api.js";
+import "../css/UserGraph.css";
+
+const { Content } = Layout;
 
 function UserGraph(props) {
   const username = props.match.params.id;
+
+  const [displayUsername, setDisplayUsername] = useState(username);
   const [userGraph, setUserGraph] = useState({ nodes: [], links: [] });
+  const [userDegrees, setUserDegrees] = useState({});
+  const [transactions, setTransactions] = useState({});
 
   useEffect(() => {
     const generateUserGraph = async () => {
-      let allUsers = new Set([username]);
+      let allUsers = new Set();
       let searched = new Set();
+      let toSearch = new Set([username]);
+      let userDegrees = {};
       let links = [];
+      let transactions = [];
 
       for (let i = 0; i < 3; i += 1) {
-        let toSearch = new Set([...allUsers].filter((x) => !searched.has(x)));
+        if (i !== 0) {
+          toSearch = new Set([...allUsers].filter((x) => !searched.has(x)));
+          for (let u of toSearch) {
+            if (!(u in userDegrees)) userDegrees[u] = i;
+          }
+        }
         searched = allUsers;
         await searchDegree(toSearch).then((data) => {
           allUsers = new Set([...allUsers, ...data[0]]);
           links.push(...data[1]);
+          transactions.push(...data[2]);
         });
       }
+
+      for (let u of allUsers) {
+        if (!(u in userDegrees)) userDegrees[u] = 3;
+      }
+
+      let realUsername = matchUsername(allUsers);
+      userDegrees[realUsername] = 0;
+      setUserDegrees(userDegrees);
+      setTransactions(transactions);
+      setDisplayUsername(realUsername);
 
       let graph = { nodes: [], links: links };
       for (let user of allUsers) graph["nodes"].push({ name: user });
       setUserGraph(graph);
     };
 
+    const matchUsername = (allUsers) => {
+      for (let u of allUsers) {
+        if (u.toLowerCase() === username.toLowerCase()) return u;
+      }
+    };
+
     const searchDegree = async (toSearch) => {
       let users = new Set();
       let links = [];
+      let transactions = [];
 
       for (let user of toSearch) {
         const data = await getUserTransactions(user);
@@ -38,16 +74,30 @@ function UserGraph(props) {
             users.add(t.sender);
             users.add(t.recipient);
             links.push({ from: t.sender, to: t.recipient });
+            transactions.push(t);
           }
         }
       }
-      return [users, links];
+      return [users, links, transactions];
     };
 
     generateUserGraph();
   }, [username]);
 
-  return <Graph graph={userGraph} />;
+  return (
+    <Layout>
+      <Sidebar
+        username={displayUsername}
+        userDegrees={userDegrees}
+        transactions={transactions}
+      />
+      <Content>
+        <div className="graph">
+          <Graph graph={userGraph} />
+        </div>
+      </Content>
+    </Layout>
+  );
 }
 
 export default UserGraph;
