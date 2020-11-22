c7c5c0d28510dff699224cd06d6e425de6bfd520
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-11-22 02:01:23 +0000

    🎩 Add user information (#7)

--- src/util/api.js
+++ src/util/api.js
@@ -4,9 +4,19 @@ const instance = axios.create({
   baseURL: "https://venmoapi.pineapple.lol",
 });
 
+export const getUserInformation = (username) => {
+  return instance.get(`/${username}`).then(
+    (res) => res.data.info,
+    (err) => {
+      console.error(err);
+      return null;
+    }
+  );
+};
+
 export const getUserTransactions = (username) => {
   return instance.get(`/${username}`).then(
-    (res) => res.data,
+    (res) => res.data.transactions,
     (err) => {
       console.error(err);
       return null;
